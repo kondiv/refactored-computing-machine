fcd55709819b823c536b56fc45c3298843cee624
kondiv <ilyakondrachin77@gmail.com>
2025-11-15 16:04:32 +0300

    use extension methods

--- src/services/EmployeesService/EmployeesService.App/Program.cs
+++ src/services/EmployeesService/EmployeesService.App/Program.cs
@@ -1,3 +1,4 @@
+using EmployeesService.App.Extensions;
 using EmployeesService.App.Infrastructure;
 using FluentValidation;
 using MassTransit;
@@ -10,28 +11,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ApplicationContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly, includeInternalTypes: true);
+builder.Services.AddFluentValidation();
 
-builder.Services.AddMediatR(config =>
-    config.RegisterServicesFromAssembly(typeof(Program).Assembly));
+builder.Services.AddMediatr();
 
-builder.Services.AddMassTransit(busConfigurator =>
-{
-    busConfigurator.SetKebabCaseEndpointNameFormatter();
-
-    busConfigurator.AddConsumers(typeof(Program).Assembly);
-
-    busConfigurator.UsingRabbitMq((context, configurator) =>
-    {
-        configurator.Host(new Uri(builder.Configuration["MessageBroker:Host"]!), h =>
-        {
-            h.Username(builder.Configuration["MessageBroker:Username"]!);
-            h.Password(builder.Configuration["MessageBroker:Password"]!);
-        });
-
-        configurator.ConfigureEndpoints(context);
-    });
-});
+builder.Services.AddRabbitMq(builder.Configuration);
 
 builder.Services.AddControllers().AddNewtonsoftJson();
 
